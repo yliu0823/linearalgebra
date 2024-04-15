64baa25c08ee25311347e4f906f7b67bf2d45235
yliu0823 <yliu177@smith.edu>
2024-04-15 08:56:40 -0400

    ..

---
+++ R/code.R
@@ -0,0 +1,11 @@
+library(pracma)
+A <- matrix(c(1,4,5,6,7,8,3,6,4),3,3,byrow=TRUE)
+rref(A)
+linear_dependent_c <- function(x){
+  x_ncol <- ncol(x)
+  x_rref <- rref(x)
+
+  for (i in 1:x_ncol){
+
+  }
+}
